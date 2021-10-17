5aec23bf884dd62ee69c7b0aba78a03e30e973aa
Ayrton Bello <59799942+aaybe@users.noreply.github.com>
2021-10-17 00:34:23 -0500

    Create practice1.py

---
+++ practice1.py
@@ -0,0 +1,13 @@
+#Exercise 1: Print First 10 natural numbers using while loop
+
+num = 0
+while (num <= 10):
+    print(num)   
+    num += 1
+    
+#Exercise 2: Print the following pattern
+#1 
+#1 2 
+#1 2 3 
+#1 2 3 4 
+#1 2 3 4 5
